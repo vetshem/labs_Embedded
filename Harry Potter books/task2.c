88656c4f387f16002bf8030fd42e9b63b11a67f3
vetshem <vetshem69@gmail.com>
2023-12-26 22:21:20 +0200

    reworked link list

--- Harry Potter books/task2.c
+++ Harry Potter books/task2.c
@@ -12,7 +12,7 @@ struct Book {
     int yearPublished;
     struct Book *next; // Вказівник на наступний елемент у списку
 };
-
+void printBook(struct Book *book);
 // Функція для створення нової книги
 struct Book *createBook(const char title[], float price, int numPages, const char language[], float weight, int yearPublished) {
     struct Book *book = (struct Book *)malloc(sizeof(struct Book));
@@ -32,7 +32,29 @@ struct Book *createBook(const char title[], float price, int numPages, const cha
     return book;
 }
 
-// Функція для виведення інформації про книги
+// Функція для додавання книги до списку
+void addToBookList(struct Book **head, struct Book *newBook) {
+    if (*head == NULL) {
+        *head = newBook;
+    } else {
+        struct Book *current = *head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = newBook;
+    }
+}
+
+// Функція для виведення інформації про всі книги в списку
+void printAllBooks(struct Book *head) {
+    struct Book *currentBook = head;
+    while (currentBook != NULL) {
+        printBook(currentBook);
+        currentBook = currentBook->next;
+    }
+}
+
+// Функція для виведення інформації про книгу
 void printBook(struct Book *book) {
     printf("Назва книги: %s\n", book->title);
     printf("Ціна: %.2f$\n", book->price);
@@ -43,40 +65,34 @@ void printBook(struct Book *book) {
     printf("\n");
 }
 
-int main() {
-    // Створення книг та додавання їх до списку
-    struct Book *firstBook = createBook("Harry Potter and the Philosopher's Stone", 9.99, 223, "English", 0.5, 1997);
-    struct Book *secondBook = createBook("Harry Potter and the Chamber of Secrets", 21.05, 251, "English", 0.6, 1998);
-    struct Book *thirdBook = createBook("Harry Potter and the Prisoner of Azkaban", 14.99, 317, "English", 0.7, 1999);
-    struct Book *fourthBook = createBook("Harry Potter and the Goblet of Fire", 20.47, 636, "English", 1.4, 2000);
-    struct Book *fifthBook = createBook("Harry Potter and the Order of the Phoenix", 18.07, 766, "English", 1.6, 2003);
-    struct Book *sixthBook = createBook("Harry Potter and the Half-Blood Prince", 20.99, 607, "English", 1.3, 2005);
-    struct Book *seventhBook = createBook("Harry Potter and the Deathly Hallows", 23.99, 607, "English", 1.3, 2007);
-
-
-    // З'єднання книг в зв'язаний список
-    firstBook->next = secondBook;
-    secondBook->next = thirdBook;
-    thirdBook->next = fourthBook;
-    fourthBook->next = fifthBook;
-    fifthBook->next = sixthBook;
-    sixthBook->next = seventhBook;
-
-
-    // Виведення інформації про всі книги в списку
-    struct Book *currentBook = firstBook;
-    while (currentBook != NULL) {
-        printBook(currentBook);
-        currentBook = currentBook->next;
-    }
-
-    // Звільнення виділеної пам'яті
-    currentBook = firstBook;
+// Функція для звільнення виділеної пам'яті
+void freeAllBooks(struct Book *head) {
+    struct Book *currentBook = head;
     while (currentBook != NULL) {
         struct Book *temp = currentBook;
         currentBook = currentBook->next;
         free(temp);
     }
+}
+
+int main() {
+    // Ініціалізація голови списку
+    struct Book *bookListHead = NULL;
+
+    // Створення та додавання книг до списку
+    addToBookList(&bookListHead, createBook("Harry Potter and the Philosopher's Stone", 9.99, 223, "English", 0.5, 1997));
+    addToBookList(&bookListHead, createBook("Harry Potter and the Chamber of Secrets", 21.05, 251, "English", 0.6, 1998));
+    addToBookList(&bookListHead, createBook("Harry Potter and the Prisoner of Azkaban", 14.99, 317, "English", 0.7, 1999));
+    addToBookList(&bookListHead, createBook("Harry Potter and the Goblet of Fire", 20.47, 636, "English", 1.4, 2000));
+    addToBookList(&bookListHead, createBook("Harry Potter and the Order of the Phoenix", 18.07, 766, "English", 1.6, 2003));
+    addToBookList(&bookListHead, createBook("Harry Potter and the Half-Blood Prince", 20.99, 607, "English", 1.3, 2005));
+    addToBookList(&bookListHead, createBook("Harry Potter and the Deathly Hallows", 23.99, 607, "English", 1.3, 2007));
+
+    // Виведення інформації про всі книги в списку
+    printAllBooks(bookListHead);
+
+    // Звільнення виділеної пам'яті
+    freeAllBooks(bookListHead);
 
     return 0;
 }
